355461d1622097eb7ce283cdaa5baf0c3aee11ec
artyomakulov <artyomakulov@gmail.com>
2021-12-08 01:46:17 +0200

    7-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
+let message = '';
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        email,
+        password,
+    }
+    if (email === '' || password === '') {
+        message = alert('заполните все поля');
+    }
+    if (email && password) {
+        console.log(formData);
+        form.reset();
+    }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const colorChangeButton = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+const changeColor = () => {
+  document.body.style.background = getRandomHexColor();
+}
+const changeColorSpan = () => {
+  colorSpan.textContent = getRandomHexColor();
+}
+
+colorChangeButton.addEventListener('click', changeColor);
+colorChangeButton.addEventListener('click', changeColorSpan);
