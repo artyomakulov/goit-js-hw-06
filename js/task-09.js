534cd978092bddbb3d9212c20fe6943c512f0cdf
artyomakulov <artyomakulov@gmail.com>
2021-12-09 20:08:20 +0200

    исправил 9 задание

--- js/task-09.js
+++ js/task-09.js
@@ -2,16 +2,51 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 const colorChangeButton = document.querySelector('.change-color');
 const colorSpan = document.querySelector('.color');
 
 const changeColor = () => {
-  document.body.style.background = getRandomHexColor();
-}
-const changeColorSpan = () => {
-  colorSpan.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = colorSpan.textContent = getRandomHexColor();
 }
 
 colorChangeButton.addEventListener('click', changeColor);
-colorChangeButton.addEventListener('click', changeColorSpan);
+
+
+
+
+// const changeColorSpan = () => {
+//   colorSpan.textContent = getRandomHexColor();
+// }
+// colorChangeButton.addEventListener('click', changeColorSpan);
+
+
+
+
+
+// const bodyEl = document.querySelector('body');
+// const btnChangeColorel = document.querySelector('.change-color');
+// const spanNameColorEl = document.querySelector('.color');
+
+// btnChangeColorel.addEventListener('click', onBtnChangeColorel);
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+// }
+// function onBtnChangeColorel(event) {
+//   bodyEl.style.backgroundColor = getRandomHexColor();
+//   spanNameColorEl.textContent =  bodyEl.style.backgroundColor;
+// }
+
+
+
+
+
+
+// const Btn = document.querySelector('.change-color');
+// const span = document.querySelector('.color');
+// Btn.addEventListener('click', () => {
+//   let colorBody = document.body.style.backgroundColor = getRandomHexColor();
+//   span.textContent = colorBody;
+// } )
+
